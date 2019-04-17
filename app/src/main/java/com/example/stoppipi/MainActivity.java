5db82183882da45744b0c93348ff747e7b0c69d3
Anita Orhand <anita.orhand@gmail.com>
2019-04-17 10:21:04 +0200

    First Version

---
+++ app/src/main/java/com/example/stoppipi/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.stoppipi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.WindowManager;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startStopPipi(View view) {
+        Intent intent = new Intent(MainActivity.this, WakeUpActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/stoppipi/WakeUpActivity.java
@@ -0,0 +1,117 @@
+package com.example.stoppipi;
+
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.FloatingActionButton;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+import devs.mulham.horizontalcalendar.HorizontalCalendar;
+import devs.mulham.horizontalcalendar.model.CalendarEvent;
+import devs.mulham.horizontalcalendar.utils.CalendarEventsPredicate;
+import devs.mulham.horizontalcalendar.utils.HorizontalCalendarListener;
+
+public class WakeUpActivity extends AppCompatActivity {
+
+    HorizontalCalendar horizontalCalendar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wake_up);
+
+        // start 2 months ago from now
+        Calendar startDate = Calendar.getInstance();
+        startDate.add(Calendar.MONTH, -10);
+        // End after 2 months from now
+        Calendar endDate = Calendar.getInstance();
+        endDate.add(Calendar.MONTH, 1);
+        // Default Date set to Today.
+        final Calendar defaultSelectedDate = Calendar.getInstance();
+
+        horizontalCalendar = new HorizontalCalendar.Builder(this, R.id.horizontalCalendarView)
+                .range(startDate, endDate)
+                .datesNumberOnScreen(7)
+                .configure()
+                    .formatTopText("EEE")
+                    .formatMiddleText("d")
+                    .formatBottomText("MMMM")
+                    .showTopText(false)
+                    .showBottomText(false)
+                    .textColor(Color.LTGRAY, Color.LTGRAY)
+                    .selectedDateBackground(new ColorDrawable(getResources().getColor(R.color.colorAccent)))
+                    .selectorColor(getResources().getColor(R.color.colorWhite))
+                    .sizeTopText(getResources().getDimension(R.dimen.text_small))
+                    .sizeMiddleText(getResources().getDimension(R.dimen.text_medium))
+                    .sizeBottomText(getResources().getDimension(R.dimen.text_small))
+                    .colorTextTop(Color.LTGRAY, Color.LTGRAY)
+                    .colorTextMiddle(Color.LTGRAY, Color.LTGRAY)
+                    .colorTextBottom(Color.LTGRAY, Color.LTGRAY)
+                .end()
+                .defaultSelectedDate(defaultSelectedDate)
+                .build();
+
+        setSelectedDateSimpleDateFormatFR(defaultSelectedDate);
+
+        horizontalCalendar.setCalendarListener(new HorizontalCalendarListener() {
+            @Override
+            public void onDateSelected(Calendar date, int position) {
+                setSelectedDateSimpleDateFormatFR(date);
+            }
+        });
+
+        TextView textViewDateFR = findViewById(R.id.textViewDateFR);
+        Typeface face = Typeface.createFromAsset(getAssets(),"fonts/Signatra.ttf");
+        textViewDateFR.setTypeface(face);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                horizontalCalendar.goToday(false);
+            }
+        });
+
+        //myCalendar = (CalendarView)findViewById(R.id.calendarView);
+        /*int daysInMonth = .getActualMaximum(Calendar.DAY_OF_MONTH);
+        cal.setMaxDate(daysInMonth);
+        /*myCalendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(CalendarView view, int y, int m, int d) {
+
+            }
+        });*/
+
+    }
+
+    public void setSelectedDateSimpleDateFormatFR(Calendar date) {
+        String selectedDateCalendarStr = DateFormat.format("EEEE-dd-MMMM-yyyy", date).toString();
+        SimpleDateFormat smfEN = new SimpleDateFormat("EEEE-dd-MMMM-yyyy", Locale.ENGLISH);
+        Date selectedDateSimpleDateFormatEN = null;
+        try {
+            selectedDateSimpleDateFormatEN = smfEN.parse(selectedDateCalendarStr);
+        }
+        catch (ParseException e) {
+            e.printStackTrace();
+        }
+        SimpleDateFormat smfFR = new SimpleDateFormat("EEEE dd MMMM yyyy", Locale.FRENCH);
+        String selectedDateSimpleDateFormatFR = smfFR.format(selectedDateSimpleDateFormatEN);
+        TextView textViewDateFR = findViewById(R.id.textViewDateFR);
+        selectedDateSimpleDateFormatFR = selectedDateSimpleDateFormatFR.substring(0,1).toUpperCase() + selectedDateSimpleDateFormatFR.substring(1);
+        textViewDateFR.setText(selectedDateSimpleDateFormatFR);
+    }
+}
+
+
